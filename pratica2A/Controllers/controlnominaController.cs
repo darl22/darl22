ceb3ed455ad1f9b2ce7cb9bcdb1d9c07c617cce2
unknown <corderodarlyn@gmail.com>
2021-08-30 10:26:01 -0400

    Signed-off-by: unknown <corderodarlyn@gmail.com>

---
+++ pratica2A/Controllers/controlnominaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pratica2A.Models;
+
+namespace pratica2A.Controllers
+{
+    public class controlnominaController : Controller
+    {
+        // GET: controlnomina
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ pratica2A/Models/nomina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pratica2A.Models
+{
+    public class nomina
+    {
+        public string nombre { get; set; } = "darlyn johermy";
+        public string apellido { get; set; } = "cordero mena";
+        public string cargo { get; set; } = "help desk";
+
+        public string salario { get; set; } = "300000";
+    }
+}
